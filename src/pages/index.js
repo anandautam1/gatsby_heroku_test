d6e1b37a67d35ed814b511e4dd693339b8efd100
anandautam1 <ananda_utama@y7mail.com>
2018-12-28 18:04:24 +1100

    Added simple code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import FullHeader from 'reuwsaat-react-full-header';
 
-export default () => <div>Hello world!</div>
+export default () => 
+    <div>
+        <FullHeader
+            title="Water is good"
+            subtitle="Water you waiting for"
+            bgColor="#EBE9EB"
+            textColor="#3299BB"
+            font="Helvetica"
+            video="http://mazwai.com/system/posts/videos/000/000/161/original/leonard_soosay--missfit.mp4?1424004876"
+        />
+        Hello world!
+    </div>
